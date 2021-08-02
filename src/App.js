7552510c8ef38bc64d4f4fc4f702a02e58b0f575
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-02 15:29:12 +0100

    added swipe buttons similar to tinder's

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css"
 import Header from "./Header"
 import TinderCards from "./TinderCards"
+import SwipeButtons from "./SwipeButtons"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           </Route>
           <Route path="/">
             <TinderCards />
+            <SwipeButtons />
           </Route>
         </Switch>
         {/* Header */}
